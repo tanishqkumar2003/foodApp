bcac290d33219234a346fe174a1b6a3b07ed6d40
tanishqkumar2003 <tanishqpayla@gmail.com>
2024-06-08 21:32:58 +0530

    shimmer

--- src/Components/Shimmer.js
+++ src/Components/Shimmer.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const Shimmer = ()=>{
     return(
         <div className="shimmer-container">
@@ -19,26 +18,4 @@ const Shimmer = ()=>{
     )
 }
 
-=======
-const Shimmer = ()=>{
-    return(
-        <div className="shimmer-container">
-            <div className="shimmer-card">Cards</div>
-            <div className="shimmer-card"></div>
-            <div className="shimmer-card"></div>
-            <div className="shimmer-card"></div>
-            <div className="shimmer-card"></div>
-            <div className="shimmer-card"></div>
-            <div className="shimmer-card"></div>
-            <div className="shimmer-card"></div>
-            <div className="shimmer-card"></div>
-            <div className="shimmer-card"></div>
-            <div className="shimmer-card"></div>
-            <div className="shimmer-card"></div>
-
-        </div>
-    )
-}
-
->>>>>>> 9e076a74a4793b5843e2eaeb0539f5ba2857bdbe
 export default Shimmer;
