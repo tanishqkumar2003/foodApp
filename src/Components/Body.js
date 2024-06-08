d79c3cb3a75c3ad0e8583d929a9aa845429b4b74
tanishqkumar2003 <tanishqpayla@gmail.com>
2024-06-08 21:27:03 +0530

    08 June 2024

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -1,25 +1,47 @@
 import RestaurantCard from "./RestaurantCard";
 import resObj from "../utils/MockData";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import Shimmer from "./Shimmer";
 
 const Body = () =>{
-    const[ListOfRestaurants, setListOfRestaurant] = useState(resObj)
-    return(
+    const[ListOfRestaurants, setListOfRestaurant] = useState([]);
+    
+    useEffect(()=>{
+        setTimeout(() => {
+            fetchData();
+        }, 2000);
+    },[]);
+
+    const fetchData = async () => {
+        const data = await fetch(
+            "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9352403&lng=77.624532&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
+
+          const json = await data.json();
+          setListOfRestaurant(json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants)
+        //   console.log(json);   
+        console.log(json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants);   
+    };
+
+    // Conditional rendering using if conndition. Same can be performed by ternary operator
+    // if(ListOfRestaurants.length === 0){
+    //     return<shimmer/>
+    // }
+
+    return (
         <div className="body">
             <div className="filter">
                 <button 
-                className="filterr-btn"
+                className="filter-btn"
                 onClick={()=>{
                     const filteredList = ListOfRestaurants.filter(
-                        (res) => res.info.avgRating > 4.5
+                        (res) => res.info.avgRating >= 4.5
                     );
                     setListOfRestaurant(filteredList);
                 }}
                 >Top Rated Restaurants</button>
             </div>
-            <div className="search">Search</div>
+            {/* <div className="search">Search</div> */}
             <div className="res-container">
-                
                 {ListOfRestaurants.map((restaurants) =>(
                     <RestaurantCard key={restaurants.info.id} resData={restaurants}/>
                 ))}
--- src/Components/app.js
+++ src/Components/app.js
@@ -4,10 +4,6 @@ import Header from "./Header";
 import Body from "./Body";
 
 
-
-
-
-
 const AppLayout = () =>{
     return(
         <div className="app">
