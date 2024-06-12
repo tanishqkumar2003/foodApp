c1a2c405a414ddff80d89edc52668724e2a20533
tanishqkumar2003 <tanishqpayla@gmail.com>
2024-06-12 18:54:41 +0530

    Changes in api fixed 12June

--- src/Components/RestaurantMenu.js
+++ src/Components/RestaurantMenu.js
@@ -1,36 +1,30 @@
 import { useState, useEffect } from "react"
-import RestaurantCard from "./RestaurantCard";
 
 const RestaurantMenu = ()=>{
    const[resInfo, setResInfo] = useState({});
    const [resMenu, setResMenu] = useState([]);
-   const {cuisines,avgRating,costForTwoMessage,name} = resInfo;
+   const {cuisines=[],avgRating,costForTwoMessage,name} = resInfo;
+   //The error "Cannot read properties of undefined (reading 'join')" occurs because cuisines is undefined when the component first renders. This happens because resInfo is initially an empty object, and cuisines is being destructured from it.
+   //To fix this issue, you can provide a default value for cuisines when destructuring it from resInfo.
 
    useEffect(()=>{
     fetchMenu();
    }, []);
 
    const fetchMenu = async ()=>{
-        const data = await fetch("https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&    complete-menu=true&lat=12.9352403&lng=77.624532&restaurantId=21954");
+        const data = await fetch("https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9352403&lng=77.624532&restaurantId=581809&catalog_qa=undefined&submitAction=ENTER");
 
         const json = await data.json();
         console.log(json);
        
         const fetchedData = json?.data?.cards[2]?.card?.card?.info;
-        setResInfo(fetchedData);
+        setResInfo(fetchedData);  
+        console.log(fetchedData);
 
-         
-        //console.log(fetchedData);
-
-        const menu = json?.data?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards[1]?.card?.card;
+        const menu = json?.data?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards[6].card.card;
         const {itemCards} = menu;
         setResMenu(itemCards);
-         console.log(itemCards);
-         // console.log(itemCards[2].card.info.name);         
-         // itemCards.map((restaurants) =>(
-         // console.log(restaurants.card.info.name)
-    
-         // ))
+         console.log(resMenu);
 
    };
 
@@ -39,14 +33,19 @@ const RestaurantMenu = ()=>{
   return (
      <div className="menu">
        <h1>{name}</h1>
-         <ul>
+       <h3>{cuisines.join(", ")}</h3>
+
+
+       <ul>
          {
             resMenu.map((mItem)=>(
                <li key={mItem.card.info.id}> {mItem.card.info.name}</li>
                // console.log(mItem.card.info.name);
             ))
          }  
-         </ul>     
+         </ul>        
+   
+
      </div>
   );
 
